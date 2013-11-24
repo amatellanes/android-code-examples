c9db7895e81279dc446ea3f4feceb1c10a3da8ee
amatellanes <dev.amatellanes@gmail.com>
2013-11-24 13:48:43 +0100

    Updating Navigation Drawer - part 2

--- NavigationDrawer/src/com/amatellanes/android/examples/MainActivity.java
+++ NavigationDrawer/src/com/amatellanes/android/examples/MainActivity.java
@@ -1,10 +1,12 @@
 package com.amatellanes.android.examples;
 
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,14 +15,16 @@ import android.widget.ListView;
 public class MainActivity extends ActionBarActivity {
 
 	private DrawerLayout drawerLayout;
+	private ActionBarDrawerToggle drawerToggle;
 	private ListView navList;
 	private CharSequence mTitle;
 
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+
+		mTitle = getTitle(); // Get current title
 
 		this.drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 		this.navList = (ListView) findViewById(R.id.left_drawer);
@@ -34,9 +38,57 @@ public class MainActivity extends ActionBarActivity {
 				android.R.layout.simple_list_item_1, names);
 		navList.setAdapter(adapter);
 		navList.setOnItemClickListener(new DrawerItemClickListener());
+		drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
+				R.drawable.ic_navigation_drawer, R.string.open_drawer,
+				R.string.close_drawer) {
+
+			/**
+			 * Called when a drawer has settled in a completely closed state.
+			 */
+			public void onDrawerClosed(View view) {
+				getSupportActionBar().setTitle(mTitle);
+				// creates call to onPrepareOptionsMenu()
+				supportInvalidateOptionsMenu();
+			}
+
+			/**
+			 * Called when a drawer has settled in a completely open state.
+			 */
+			public void onDrawerOpened(View drawerView) {
+				getSupportActionBar().setTitle("Selecciona opción");
+				// creates call to onPrepareOptionsMenu()
+				supportInvalidateOptionsMenu();
+			}
+		};
+
+		// Set the drawer toggle as the DrawerListener
+		drawerLayout.setDrawerListener(drawerToggle);
 
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	/**
+	 * Called whenever we call invalidateOptionsMenu()
+	 */
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		// If the navigation drawer is open, hide action items related to the
+		// content view
+		boolean drawerOpen = drawerLayout.isDrawerOpen(navList);
+		menu.findItem(R.id.action_search).setVisible(!drawerOpen);
+		return super.onPrepareOptionsMenu(menu);
+	}
+
+	/**
+	 * Callback method to be invoked when an item in the navigation drawer has
+	 * been clicked.
+	 */
 	private class DrawerItemClickListener implements
 			ListView.OnItemClickListener {
 		@Override
@@ -46,7 +98,12 @@ public class MainActivity extends ActionBarActivity {
 		}
 	}
 
-	/** Swaps fragments in the main content view */
+	/**
+	 * Swaps fragments in the main content view.
+	 * 
+	 * @param position
+	 *            selected option
+	 */
 	private void selectItem(int position) {
 		// Get text from resources
 		String option = getResources().getStringArray(R.array.nav_options)[position];
